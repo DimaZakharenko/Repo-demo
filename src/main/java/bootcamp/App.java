65edb7522a4e4aeb35d1d4bf80dcf22ca2bb8a7b
Dima Z <48586191+DimaZakharenko@users.noreply.github.com>
2019-05-07 18:41:27 +0200

    Add small change to app.java

--- src/main/java/bootcamp/App.java
+++ src/main/java/bootcamp/App.java
@@ -3,5 +3,6 @@ package bootcamp;
 public class App {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        System.out.println("Small change");
     }
 }
