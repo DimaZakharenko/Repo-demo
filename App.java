c85f05c06837c682a7b1ee39bd689cf38754016d
Dima Z <48586191+DimaZakharenko@users.noreply.github.com>
2019-05-09 17:51:07 +0200

    Add files via upload

---
+++ App.java
@@ -0,0 +1,41 @@
+package module1.classwork;
+
+public class App {
+    //varibles
+        //basic datatypes
+            //numeric
+                //hole real numbers
+                    byte byteNumber = -128; //default value ia 0
+
+                    short number;
+
+                    int intNumber;
+
+                    long longNumber;
+
+                //decimals
+                    float floatNumber;
+
+                    double doubleNumber;
+
+                //text
+                    String text; //default value ia NULL
+
+                //booleans
+                    boolean booleanValue;  //default value ia false
+
+                //characters
+                    char character;  //default value ia 0
+
+    //method(function)
+    public static void main(String[] args) {
+
+        System.out.println();
+    }
+
+    public void method1() {
+        longNumber = byteNumber + 128 + number;
+        text = "some text here";
+
+    }
+}
