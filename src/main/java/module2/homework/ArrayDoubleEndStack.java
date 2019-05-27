adcee2fb36b8bcf6dff263ec085a054a4a05609d
Dima Z <48586191+DimaZakharenko@users.noreply.github.com>
2019-05-27 16:42:03 +0200

    Update ArrayDoubleEndStack.java

--- src/main/java/module2/homework/ArrayDoubleEndStack.java
+++ src/main/java/module2/homework/ArrayDoubleEndStack.java
@@ -1,104 +1,53 @@
-package module3.classwork;
+package module2.homework;
 
 import java.util.Arrays;
 
-public class ControlStatements {
+public class ArrayDoubleEndStack {
 
-    public static void main(String[] args) {
+    private int[] doubleEndStack = new int[10];
+    private int rightPosition = doubleEndStack.length / 2;
+    private int leftPosition = doubleEndStack.length / 2 - 1;
 
-        int[] array = {1, 2, 3, 4, 5, 6, 6, 8, 9, 10};
-        printEvents(array);
-        reverseArray(array);
-        sumOfArrayElements(array);
-
-
-    }
-
-
-    static void printEvents(int[] array) {
-
-        System.out.println("Even numbers: ");
-
-        for (int i = 1; i < array.length; i++) {
-            if (array[i] % 2 == 0) {
-                System.out.println(array[i]);
-            }
-        }
-    }
-
-    static void reverseArray(int[] array) {
-
-        System.out.println("Reversed array: ");
-
-
-        for (int i = 0; i < array.length / 2; i++) {
-            int temporaryVar = array[i];
-            array[i] = array[array.length - i - 1];
-            array[array.length - i - 1] = temporaryVar;
-        }
-        System.out.println(Arrays.toString(array));
-    }
-
-    static void sumOfArrayElements(int[] array) {
-
-        System.out.println("Sum of array elements: ");
-        int arrSum = 0;
-
-        for (int i = 0; i < array.length; i++) {
-
-            arrSum += array[i];
-        }
-        System.out.println(arrSum);
-    }
-
-
-    void operationsWithConditionals() {
-        boolean condition1 = false;
-        boolean condition2 = false;
-
-        if (condition1) {
-            System.out.println("is is true");
-        } else if (condition2) {
-            System.out.println("is is true");
+    void addLeft(int number) {
+        if (leftPosition < 0) {
+            System.out.println("Double_End_Stack is full from the left side.");
+            printStack();
         } else {
-            System.out.println("is is false");
+            doubleEndStack[leftPosition--] = number;
         }
-
-        int a = 0;
-        int b = 1;
-
-        if (a == b) {
-            System.out.println("a = b");
-        } else if (a > b) {
-            System.out.println("a > b");
-        } else if (a <= b) {
-            System.out.println("a <= b");
-        } else if (a % 2 > 0) {
-            System.out.println("a is odd");
-        }
-
-        switch (a) {
-            case 0:
-                System.out.println(0);
-                break;
-            case 1:
-                System.out.println(1);
-                break;
-            default:
-                break;
-        }
-
     }
 
-    void whileCondition() {
-//
-//        while (true) {
-//            System.out.println();
-//        }
+    void addRight(int number) {
+        if (rightPosition > doubleEndStack.length - 1) {
+            System.out.println("Double_End_Stack is full from the right side.");
+            printStack();
+        } else {
+            doubleEndStack[rightPosition++] = number;
+        }
+    }
 
-        do {
-            System.out.println();
-        } while (false);
+    int removeLeft() {
+        if (leftPosition >= doubleEndStack.length / 2 - 1) {
+            System.out.println("Double_End_Stack is empty from from the left side.");
+            printStack();
+        } else {
+            doubleEndStack[++leftPosition] = 0;
+        }
+        return 0;
+    }
 
+    int removeRight() {
+        if (rightPosition <= doubleEndStack.length / 2) {
+            System.out.println("Double_End_Stack is empty from the right side.");
+            printStack();
+        } else {
+            doubleEndStack[--rightPosition] = 0;
+        }
+        return 0;
+    }
+
+    void printStack() {
+        System.out.println(Arrays.toString(doubleEndStack));
+        System.out.println();
     }
 }
