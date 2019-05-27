9c587004239c87a4a8c68c998c8de8830051771d
Dima Z <48586191+DimaZakharenko@users.noreply.github.com>
2019-05-27 16:35:10 +0200

    Create InsertionSortReversed.java
    2nd part of homework-3

---
+++ src/main/java/bootcamp/module3/homework/InsertionSortReversed.java
@@ -0,0 +1,33 @@
+package module3.homework;
+
+public class InsertionSortReversed {
+
+    public static int[] insertionSortReversed(int[] unsorted) {
+        
+        boolean swap = true;
+        do {
+            swap = false;
+            for (int i = 0; i < unsorted.length - 1; i++) {
+
+                if (unsorted[i] < unsorted[i + 1]) {
+                    int temp = unsorted[i + 1];
+                    unsorted[i + 1] = unsorted[i];
+                    unsorted[i] = temp;
+                    swap = true;
+                }
+
+                for (int j = i; j > 0; j--) {
+                    if (unsorted[j - 1] < unsorted[j]) {
+                        int temp = unsorted[j];
+                        unsorted[j] = unsorted[j - 1];
+                        unsorted[j - 1] = temp;
+                        swap = true;
+                    }
+                }
+            }
+
+        } while (swap);
+
+        return unsorted;
+    }
+}
