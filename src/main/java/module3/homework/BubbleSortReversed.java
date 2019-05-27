589ada683a042eb518fc02831c691cb666d94a4d
Dima Z <48586191+DimaZakharenko@users.noreply.github.com>
2019-05-27 17:06:56 +0200

    Create BubbleSortReversed.java

---
+++ src/main/java/module3/homework/BubbleSortReversed.java
@@ -0,0 +1,23 @@
+package module3.homework;
+
+public class BubbleSortReversed {
+
+    public static int[] bubbleSortReversed(int[] unsorted) {
+
+        boolean swap;
+        do {
+            swap = false;
+            for (int i = 0; i < unsorted.length - 1; i++) {
+
+                if (unsorted[i] < unsorted[i + 1]) {
+                    int temp = unsorted[i];
+                    unsorted[i] = unsorted[i + 1];
+                    unsorted[i + 1] = temp;
+                    swap = true;
+                }
+            }
+        } while (swap);
+
+        return unsorted;
+    }
+}
