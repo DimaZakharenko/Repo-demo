454576830c4982c0f0d0e99c28ffde45e1a4856d
Dima Z <48586191+DimaZakharenko@users.noreply.github.com>
2019-05-27 16:44:32 +0200

    Created SelectionSortReversed.java

---
+++ src/main/java/bootcamp/module3/homework/SelectionSortReversed.java
@@ -0,0 +1,31 @@
+package module3.homework;
+
+public class SelectionSortReversed {
+
+
+    public static int[] selectionSortReversed(int[] unsorted) {
+
+        for (int i = 1; i < unsorted.length; i++) {
+
+            if (i == unsorted.length - 1 && (unsorted[i - 1] <= unsorted[i])) {
+                break;
+            }
+
+            for (int j = unsorted.length - 1; j > i; j--) {
+                if (unsorted[j - 1] < unsorted[j]) {
+                    int temp = unsorted[j];
+                    unsorted[j] = unsorted[j - 1];
+                    unsorted[j - 1] = temp;
+                }
+            }
+
+            if (unsorted[i - 1] < unsorted[i]) {
+                int temp = unsorted[i];
+                unsorted[i] = unsorted[i - 1];
+                unsorted[i - 1] = temp;
+            }
+
+        }
+        return unsorted;
+    }
+}
